751bf316f188f20f87d0da31e43bf2c331da190e
Rob Crozier <doyouhavehotsauce@gmail.com>
2022-07-13 21:27:42 -0400

    js reps

--- javascript-module/ex-1.js
+++ javascript-module/ex-1.js
@@ -4,23 +4,22 @@
 //number of pages, and whether or not you have read the book.
 //Put a function into the constructor that can report the book info like so:
 function BookDetails(title, author, pageNum, status){
-    constructor()
     this.title = title;
     this.author = author;
     this.pageNum = pageNum;
     this.status = status;
-
-    info() {
-        return `${this.title} by ${this.author}, ${this.pageNum} pages, ${this.status}`
+    this.cheer = function(){
+        console.log('yeeah')
     }
+
+    // info() {
+    //     return `${this.title} by ${this.author}, ${this.pageNum} pages, ${this.status}, ${this.cheer}`
+    // }
 }
 
-BookDetails.prototype.info = function(){
-}
+const theHobbit = new BookDetails('The Hobbit', 'JRR Tolkien', '295', false)
 
-const theHobbit = new BookDetails('The Hobbit', 'JRR Tolkien', '295', 'not read yet')
-
-theHobbit.info() // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+theHobbit.info() // "The Hobbit by J.R.R. Tolkien, 295 pages, false"
 
 
 //Note: It is almost always best to return things rather than 
@@ -28,3 +27,51 @@ theHobbit.info() // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 //return the info string and log it after the function has been called:
 
 console.log(theHobbit.info());
+
+
+
+
+//Practice
+function makeDogs(name, breed, isGoodBoy){
+    this.name = name;
+    this.breed = breed;
+    this.isGoodBoy = isGoodBoy;
+    this.speak = function(){
+        console.log('wooof')
+    }
+}
+
+const dog = new makeDogs('Tod', 'pitbull', true);
+const dog2 = new makeDogs('Walter', 'pommerainiananan', false);
+
+console.log(`my name is ${dog.name} and my friend is ${dog2.name}. it is ${dog2.isGoodBoy} that he is a good boy`)
+
+
+
+
+
+function whosAGoodBoy(dog){
+    return (dog.isGoodBoy ? 'yes he is' : 'nope')
+}
+
+whosAGoodBoy(dog2)
+whosAGoodBoy(dog)
+
+
+
+
+
+
+
+function Player(name, marker) {
+    this.name = name
+    this.marker = marker
+    this.sayName = function() {
+      console.log(name)
+    }
+  }
+  
+  const player1 = new Player('steve', 'X')
+  const player2 = new Player('also steve', 'O')
+  player1.sayName() // logs 'steve'
+  player2.sayName() // logs 'also steve'
