ba26d45554588d0de6710c8a7df82f4dc83dd6c7
Rob Crozier <doyouhavehotsauce@gmail.com>
2022-07-29 21:26:08 -0400

    created book modal and adjusted styling

--- project-library/script.js
+++ project-library/script.js
@@ -1,38 +1,120 @@
-//-------------------------------------------//
-let garage = [
-]
+window.onload = function(){
 
-function Car(brand, model, year, color){
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.color = color;
-    this.horn = function(){
-        console.log('beep beep mother fucker')
+    let library = [];
+    let btn = document.getElementById('bookSubmit');
+    let cardContainer = document.getElementById('wrap');
+    let addBookBtn = document.getElementById('add-book');
+    let grabWrap = document.querySelector('div.add-book-wrap');
+
+    let selectedCard
+
+
+    //done - get the values of the user input when submit click
+    //create Add book button - on click form modal appears
+    //ensure aria compliance on form & cards
+    //done - create a book object from that data
+    //done - store the book object in an array
+    //done - print the array of books to the page
+        //add toggleable feature to read/unread
+
+    //if user deletes a book remove it from the array
+        //add confirmation step feature
+    
+    //done - if user adds an book add to page without refreshing page
+
+
+
+//creates book object
+    function Book(title, author){
+        this.title = title;
+        this.author = author;
+        // this.readStatus = readStatus;
+        this.addToLibrary = function(){
+            library.push(this);
+        };
+
+        this.addToLibrary();
+        
+        showBookForm();
+        clearContainer();
+        showBooks();
+
     }
-    this.addToGarage = function(){
-        garage.push(this);
-    };
-
-    this.addToGarage();
-
-}
-
-const car1 = new Car('buick', 'station wagon', 1983, 'wood')
-const car2 = new Car('volkswagon', 'beetle', 1996, 'orange')
-const car3 = new Car('honda', 'civic', 2020, 'blue')
-const car4 = new Car('dodge', 'durango', 1999, 'black')
-const car5 = new Car('audi', 's7', 1999, 'silver')
 
 
 
-function showCars(){
-    for(car of garage) {
-        document.querySelector('#wrap').innerHTML
-        += '<article>'+
-        `<h3>${car.brand}</h3>`+
-        `<p>model: ${car.model} year: ${car.year} color: ${car.color}</p>`+
-        `</article>`;
+
+//puts cards on page
+    function showBooks(){
+        for(book of library) {
+            document.getElementById('wrap').innerHTML
+            += '<article class="card">'+
+            `<h3>${book.title}</h3>`+
+            `<p>author: ${book.author} <br>`+
+            `completed: <span class="deleteBook">delete</span>`+
+            `</p>`+
+            `</article>`;
+        }
     }
-}
-showCars();
+   
+
+
+    //Event Listners
+    btn.addEventListener('click', btnHeard);
+    function btnHeard(e){
+        e.preventDefault();
+
+        let title = document.getElementById('title').value;
+        let author = document.getElementById('author').value;
+        //let readStatus = document.getElementById('read').value
+
+        let makeNewBook = new Book(title, author);
+
+        return makeNewBook
+
+       }
+
+    cardContainer.addEventListener('click', e => {
+        if(e.target.className === 'deleteBook'){
+            deleteCard();
+        }
+    })
+    
+    addBookBtn.addEventListener('click', showBookForm)
+    function showBookForm(){
+        if(grabWrap.classList.contains('hide') ){
+            grabWrap.classList.add('show');
+            grabWrap.classList.remove('hide');
+        } else {
+            grabWrap.classList.add('hide');
+            grabWrap.classList.remove('show');            
+        }
+    }
+        
+
+
+
+
+
+
+
+
+    //Helper Functions
+    function clearContainer(){
+        document.getElementById('wrap').innerHTML = "";
+    }
+    function deleteCard(){
+        console.log('delete card func called')
+    }
+
+
+
+
+    
+    } //close window on ready
+
+
+
+
+
+
